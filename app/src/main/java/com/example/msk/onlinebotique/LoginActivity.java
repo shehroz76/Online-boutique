7df6f1a486edd9e2f3ae8928afe2276242b4d247
Shehroz76 <shehroz076@gmail.com>
2017-02-14 16:42:08 +0500

    Login-signUp xml files added

---
+++ app/src/main/java/com/example/msk/onlinebotique/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.msk.onlinebotique;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MSk on 14/02/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+
+
+
+
+    
+}
